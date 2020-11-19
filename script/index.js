ffd970b70050bab9205df811f79910087dec6fab
spokeepfat007 <Spokeepfat@mail.ru>
2020-11-19 21:38:28 +0700

    fix bag with international flag

--- script/index.js
+++ script/index.js
@@ -1,15 +1,21 @@
 const input1 = document.getElementById("input");
 input1.value[input1.value.length - 1].color = 'black';
 let zero = document.getElementById("zero");
+let k = 0;
 
 function add(e) {
+    if (input1.value.length >= 2) {
+        if (input1.value[input1.value.length - 2].match(/[/*+-]/) && e === '.') return;
+    }
     if (input1.value.length === 1 && input1.value === '0' && e.match(/[0-9]/)) {
         input1.value = e;
         zero.innerText = e;
     } else if (input1.value.length === 1 && input1.value === '0' && e.match(/[.]/)) {
+        if (e === '.' && k !== 1) k = 1; else if (e === '.') return;
         input1.value += e;
         zero.innerText += e;
-    }  else if (e.match(/[0-9.]/)) {
+    } else if (e.match(/[0-9.]/)) {
+        if (e === '.' && k !== 1) k = 1; else if (e === '.') return;
         input1.value += e;
         if (input1.value[input1.value.length - 2].match(/[0-9.]/)) {
             zero.innerText += e;
@@ -17,14 +23,20 @@ function add(e) {
             zero.innerText = e
         }
     } else {
+        if (input1.value.length >= 2) {
+            if (input1.value[input1.value.length - 2].match(/[/*+-]/) || input1.value[input1.value.length - 1] === '.') return;
+        }
         input1.value += ' ' + e + ' ';
         zero.innerText = " " + e + " ";
+        k = 0;
     }
+
 }
 
 function cl() {
     input1.value = "0";
     zero.innerText = "0";
+    k = 0;
 }
 
 function del() {
@@ -34,15 +46,21 @@ function del() {
     } else if (input1.value[input1.value.length - 1].match(/[0-9.]/)) {
         input1.value = input1.value.substring(0, input1.value.length - 1);
         let i = 1;
+        k = 0;
+        if (input1.value[input1.value.length - i] === '.') k = 1;
         while (input1.value[input1.value.length - i] !== " " && (i) <= input1.value.length) {
             i++;
+            if (input1.value[input1.value.length - i] === '.') k = 1;
         }
         zero.innerText = input1.value.substring(input1.value.length - i + 1, input1.value.length);
     } else {
         input1.value = input1.value.substring(0, input1.value.length - 3)
-        let i = 1;
+        i = 1;
+        k = 0;
+        if (input1.value[input1.value.length - i] === '.') k = 1;
         while (input1.value[input1.value.length - i] !== " " && (i) <= input1.value.length) {
             i++;
+            if (input1.value[input1.value.length - i] === '.') k = 1;
         }
         zero.innerText = input1.value.substring(input1.value.length - i + 1, input1.value.length);
     }
